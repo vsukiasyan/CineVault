572422acd9ab7a273a9d36e5690345de158a576e
Vic Sukiasyan <vsukiasyan1@gmail.com>
2023-08-12 07:45:30 -0700

    Initial Commit

---
+++ CineVault/CineVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineVaultApp.swift
+//  CineVault
+//
+//  Created by Vachagan Sukiasyan on 8/12/23.
+//
+
+import SwiftUI
+
+@main
+struct CineVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
